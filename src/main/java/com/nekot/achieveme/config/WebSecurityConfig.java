cd494b4253a585bebb552be695477ab118e2a102
Зибницкий Вадим Вадимович <vv.zibnitsky@cdek-global.com>
2023-10-03 14:49:27 +0700

    TO-DO Finish registration process

--- src/main/java/com/nekot/achieveme/config/WebSecurityConfig.java
+++ src/main/java/com/nekot/achieveme/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.nekot.achieveme.config;
+package com.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +11,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.nekot.achieveme.services.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
---
+++ src/main/java/com/nekot/achieveme/controller/RegistrationController.java
@@ -0,0 +1,17 @@
+package com.nekot.achieveme.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.WebRequest;
+
+import com.nekot.achieveme.models.AchievemeUserDTO;
+
+public class RegistrationController {
+  
+  @GetMapping("/user/registration")
+  public String showRegistrationForm(WebRequest request, Model model) {
+    var userDto = new AchievemeUserDTO();
+    model.addAttribute("user", userDto);
+    return "registration";
+  }
+}
